7d7120d0734120770a8324e70f3b9923f871fae3
matthewkingdesign <hello@matthewking.design>
2018-10-19 02:13:49 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $('#name').animate({
+    opacity: '1',
+    letterSpacing:'1px',
+    marginLeft:'5%'
+  },1200);
+  $('#title').delay(500).animate({
+    opacity: '1',
+    letterSpacing:'1px',
+    marginLeft:'5%'
+  },1200);
+  $('#copy').delay(700).animate({
+    opacity: '1',
+    letterSpacing:'1px',
+    marginLeft:'5%'
+  },1200);
+  $('#copy2').delay(900).animate({
+    opacity: '1',
+    letterSpacing:'1px',
+    marginLeft:'5%'
+  },1200);
+  $('#copy3').delay(1100).animate({
+    opacity: '1',
+    letterSpacing:'1px',
+    marginLeft:'5%'
+  },1200);
+});
